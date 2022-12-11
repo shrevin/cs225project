28aaa35a95600b99635ba854780d16a299a23f09
nikhilapuppala <puppala4@illinois.edu>
2022-12-11 04:10:22 +0000

    finished dijkstras

--- build/test_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/workspaces/cs225_fixed_10_12/release-f22/cs225project/build/test_tests-b12d07c.cmake")
-  include("/workspaces/cs225_fixed_10_12/release-f22/cs225project/build/test_tests-b12d07c.cmake")
-else()
-  add_test(test_NOT_BUILT-b12d07c test_NOT_BUILT-b12d07c)
-endif()
--- build/test_tests-b12d07c.cmake
+++
@@ -1,7 +0,0 @@
-add_test( test_basic /workspaces/cs225_fixed_10_12/release-f22/cs225project/build/test test_basic  )
-set_tests_properties( test_basic PROPERTIES WORKING_DIRECTORY /workspaces/cs225_fixed_10_12/release-f22/cs225project/build)
-add_test( test_empty_line /workspaces/cs225_fixed_10_12/release-f22/cs225project/build/test test_empty_line  )
-set_tests_properties( test_empty_line PROPERTIES WORKING_DIRECTORY /workspaces/cs225_fixed_10_12/release-f22/cs225project/build)
-add_test( test_medium /workspaces/cs225_fixed_10_12/release-f22/cs225project/build/test test_medium  )
-set_tests_properties( test_medium PROPERTIES WORKING_DIRECTORY /workspaces/cs225_fixed_10_12/release-f22/cs225project/build)
-set( test_TESTS test_basic test_empty_line test_medium)
