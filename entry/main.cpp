b44dc1ba44dbe6fa8ee7dd4156216744af0eec45
aashiagrawal <aashia2@illinois.edu>
2022-12-11 23:11:26 +0000

    added iterativedfs commandline args

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "graph.hpp"
+#include "iterativedfs.hpp"
 
 int main() {
     // Graph g = Graph();
@@ -8,9 +9,28 @@ int main() {
     std::string filepath;
     std::cout << "Enter a filepath for your graph data (no quotes): ";
     std::cin >> filepath;
-    std::cout << filepath << std::endl;
+    //std::cout << filepath << std::endl;
+    std::cout <<"Adjacency List: " <<std::endl;
+    std::cout<< "" <<std::endl;
     Graph g = Graph();
     g.readCSV(filepath);
     g.printGraph();
+    std::cout<< "" <<std::endl;
+    Vertex source;
+    std::cout << "Enter a source subreddit: ";
+    std::cin >> source;
+
+    Vertex target;
+    std::cout << "Enter a target subreddit: ";
+    std::cin >> target;
+
+    int depth;
+    std::cout << "Enter a depth amount: ";
+    std::cin >> depth;
+
+
+    IterativeDfs d = IterativeDfs(filepath, source, target, depth);
+    d.printDFSResults();
+
     return 0;
 }
