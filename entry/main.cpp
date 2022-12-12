c4d1fa478587f1245d52fe373327b23be32b1f59
nikhilapuppala <puppala4@illinois.edu>
2022-12-12 22:47:34 +0000

    update dijkstras command line prompts

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "graph.hpp"
 #include "iterativedfs.hpp"
-#include "bfs.hpp"
+#include "dijkstra.hpp"
 
 int main() {
     // Graph g = Graph();
@@ -41,15 +41,27 @@ int main() {
         IterativeDfs d = IterativeDfs(filepath, source, target, depth);
         d.printDFSResults();
     } else if (traversal == "BFS") {
-        std::string filepath_to_write_to;
-        std::cout << "Enter a filepath to the file you want to store the output of BFS: ";
-        std::cin >> filepath_to_write_to;
-        BFS b = BFS(filepath);
-        std::set<Vertex> l = b.BFSWholeGraph(filepath_to_write_to);
-        std::cout << "Your output has been generated! Please navigate to " << filepath_to_write_to << std::endl;
+        std::cout<<"hello"<<std::endl;
     } else if (traversal == "Dijkstras") {
+        Vertex source;
+        std::cout << "Enter a source subreddit: ";
+        std::cin >> source;
+
+        Vertex target;
+        std::cout << "Enter a target subreddit: ";
+        std::cin >> target;
+
+        Dijkstra d = Dijkstra(g, source, filepath);
+        d.getShortestDistance(target);
+        std::vector<std::string> path = d.getShortestPath(target);
+        std::cout << std::endl;
+        std::cout << "Here are the nodes in your shortest path traversal!" << std::endl;
+        std::cout << std::endl;
+        for (size_t i = 0; i < path.size(); i++) {
+        std::cout << "Node " << i << " = " << path[i] << std::endl; 
+        }
     } else {
-        std::cout << "Unrecognizable selection! Run main again. " << std::endl;
+        std::cout << "Unrecognizable selection!" << std::endl;
     }
     
     return 0;
